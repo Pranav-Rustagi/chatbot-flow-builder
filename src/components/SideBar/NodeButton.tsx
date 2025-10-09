afb6eb5cc6283ae19034601b5cb396047c53fee8
Pranav-Rustagi <pranav001100@gmail.com>
2025-10-09 17:34:21 +0530

    Added favicon + styled buttons

--- src/components/SideBar/NodeButton.tsx
+++ src/components/SideBar/NodeButton.tsx
@@ -6,8 +6,8 @@ interface NodeButtonProps {
 
 const NodeButton: React.FC<NodeButtonProps> = ({ label = "Node", icon = null, type = "default" }) => {
     return (
-        <button className="border border-primary text-primary rounded w-full flex flex-col gap-2 items-center justify-center px-4 py-2 text-sm cursor-grab">
-            {icon && <img src={icon} alt={type} className="w-8 h-8 inline-block" />}
+        <button draggable className="border border-primary text-primary rounded bg-white w-full flex flex-col gap-2 items-center justify-center px-4 py-2 text-sm cursor-grab">
+            {icon && <img draggable="false" src={icon} alt={type} className="w-8 h-8 inline-block" />}
             <span>{label}</span>
         </button>
     )
